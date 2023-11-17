afa4c8f1fd6a7f65a9a0205d4cc033b67d8a8268
HKEDU <HKEDU@DESKTOP-OFT9IJ9>
2023-11-17 15:51:15 +0900

    command 작성중

---
+++ 19_testgitproject_csy/src/main/java/com/px/board/command/AddUserCommand.java
@@ -0,0 +1,90 @@
+package com.px.board.command;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class AddUserCommand {
+	
+	@NotBlank(message = "아이디를 입력하세요")
+	private String id;
+	
+	@NotBlank(message = "이름을 입력하세요")
+	private String name;
+	
+	@NotBlank(message = "비밀번호를 입력하세요")
+	@Length(min = 4, max = 16, message = "4자리 이상, 16자 이하로 입력하세요!")
+	private String password;
+	
+	@NotBlank(message = "전화번호를 입력하세요")
+	private String phone;
+	
+	@NotBlank(message = "이메일을 입력하세요")
+	private String email;
+
+	public AddUserCommand() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AddUserCommand(@NotBlank(message = "아이디를 입력하세요") String id, @NotBlank(message = "이름을 입력하세요") String name,
+			@NotBlank(message = "비밀번호를 입력하세요") @Length(min = 4, max = 16, message = "4자리 이상, 16자 이하로 입력하세요!") String password,
+			@NotBlank(message = "전화번호를 입력하세요") String phone, @NotBlank(message = "이메일을 입력하세요") String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.phone = phone;
+		this.email = email;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "AddUserCommand [id=" + id + ", name=" + name + ", password=" + password + ", phone=" + phone
+				+ ", email=" + email + "]";
+	}
+}
+
+
+
+
---
+++ 19_testgitproject_csy/src/main/java/com/px/board/config/ExceptionHandle.java
@@ -0,0 +1,22 @@
+package com.px.board.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandle {
+	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	// Exception이 발생하면 실행시킬 메서드
+	@ExceptionHandler(Exception.class)
+	public String handleException(Exception e, Model model) {
+		logger.error("Exception 발생 : {}", e.getMessage());
+		model.addAttribute("msg", "오류를 발생하여 확인중입니다.");
+		return "error";
+	}
+
+}
