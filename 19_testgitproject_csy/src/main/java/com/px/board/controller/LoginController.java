173b9da50831f8020d14060c792868dc7ab27f9f
HKEDU <HKEDU@DESKTOP-T8FLAON>
2023-11-20 09:58:52 +0900

    update

--- 19_testgitproject_csy/src/main/java/com/px/board/controller/LoginController.java
+++ 19_testgitproject_csy/src/main/java/com/px/board/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class LoginController {
 
 Logger logger=LoggerFactory.getLogger(getClass());
 	
