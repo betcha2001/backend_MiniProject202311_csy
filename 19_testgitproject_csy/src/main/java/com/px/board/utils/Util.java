f950606953bdb6e139fdff78553ce5e40bdb27a7
HKEDU <HKEDU@DESKTOP-OFT9IJ9>
2023-11-21 08:30:57 +0900

    update

--- 19_testgitproject_csy/src/main/java/com/px/board/utils/Util.java
+++ 19_testgitproject_csy/src/main/java/com/px/board/utils/Util.java
@@ -11,12 +11,12 @@ import com.px.board.dtos.CalDto;
 @Component
 public class Util {
 
-	//한자리를 두자리로 변환
+	// 한 자리를 두 자리로 변환
 	public static String isTwo(String str) {
 		return str.length()<2?"0"+str:str; // 5 -> "05", 10 -> "10"
 	}
 	
-	//mdate: 날짜 형식 변환 -> yy-MM-dd HH:mm
+	// mdate: 날짜 형식 변환 -> yy-MM-dd HH:mm
 	public String toDates(String mdate) {
 		//Date 형식: "yyyy-MM-dd HH:mm:ss"
 		String m=mdate.substring(0,4)+"-"
@@ -31,6 +31,7 @@ public class Util {
 		sdf.format(tm);// 문자열 타입의 경우 date타입으로 변환해서 사용해야 한다.
 		return sdf.format(tm);
 	}
+	
 	//요일별 날짜 색깔 적용하기 : 파라미터 -i, dayOfWeek 필요
 	//(공백수+현재일)%7==0 토요일
 	//(공백수+현재일)%7==1 일요일
@@ -52,6 +53,10 @@ public class Util {
 		int ed = Integer.parseInt(enddate);
 		
 		for (int j = 0; j < clist.size(); j++) {
+			// 예를 들어 sd = "01" -> 1 / ed = "03" -> 3 => Integer.parseInt()를 이용해 정수로 형변환
+			// 그러나 안 되면 IndexOf("찾을 특정 문자") 이용해서 자를 것
+			// if(1 <= d && 3 >= d) {}
+			
 			if(sd <= Integer.parseInt(clist.get(j).getStartdate().substring(6,8)) &&
 					ed >= Integer.parseInt(clist.get(j).getEnddate().substring(6,8))) {
 				calList+="<p>"
