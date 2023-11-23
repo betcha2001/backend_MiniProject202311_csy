27e0b261f3feb7a014b064114e2a8dc72ee21fa5
HKEDU <HKEDU@DESKTOP-T8FLAON>
2023-11-23 13:29:39 +0900

    update...

--- 19_testgitproject_csy/src/main/java/com/px/board/controller/MemController.java
+++ 19_testgitproject_csy/src/main/java/com/px/board/controller/MemController.java
@@ -135,7 +135,7 @@ public class MemController {
 			session.setAttribute("dto", dto);
 			session.setMaxInactiveInterval(10*60);
 			
-			//회원 등급에 따라 메인 페이지 이동 안돼.....
+			//회원 등급에 따라 메인 페이지 이동 안돼.............
 			if(dto.getGrade().toUpperCase().equals("ADMIN")) {
 				return "cal/calendar_ADMIN";
 			}else if(dto.getGrade().toUpperCase().equals("USER")) {
